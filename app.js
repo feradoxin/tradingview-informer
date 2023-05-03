e29c226a58299c8c4b497271e30d0798920582c1
feradoxin <sam.wong@protonmail.ch>
2023-05-04 03:26:59 +0800

    add short logic for margin trade

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const ccxt = require("ccxt");
 
 /** TRADE SETTINGS
  * Change the following to suit your trade market.
- * Leverage long/short param: margin leverage level
- * Quote Asset: Asset to buy/sell, e.g. ETH, BTC, BAT
- * Base Asset: Currency used to buy/sell, e.g. USDT, USDC, BTC
  */
-const leverageLong = 1;
-const leverageShort = 1;
-const quoteAsset = 'ETH';
-const baseAsset = 'USDT';
-const riskFactor = 1;
-const takeProfitFactor = 2;
+
+const longLeverage = 1;			// Leverage on long orders
+const shortLeverage = 1; 		// Leverage on short orders
+const quoteAsset = 'ETH';		// Asset to buy/sell, e.g. ETH, BTC, BAT
+const baseAsset = 'USDT';		// Currency asset used to buy/sell, e.g. USDT, USDC, BTC
+const riskFactor = 1;			// Risk:Reward risk value
+const takeProfitFactor = 2;		// Risk:Reward reward value
+const marginMode = 'isolated';	// Margin market type: 'isolated' or 'cross'
+const tick = 10;				// Update interval in seconds, default 10s to reduce API rate
 
 
 // // Telegram bot (@frdx_tv_sig_bot)
-const tgbot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const tgbot = new Telegraf(process.env.TELEGRAM_TOKEN);
 const chatId = process.env.TELEGRAM_CHATID;
 
 
@@ -85,14 +85,18 @@ let balanceBase;
 let balanceQuote;
 let totalValue;
 let initialValue;
+let marginBalanceBase;
+let marginBalanceQuote;
+let marginInitialValue;
+let marginTotalValue;
 const symbol = quoteAsset + '/' + baseAsset;
-let borrowedQuoteAmount;
 let signal1;
 let signal2;
 let longPosition;
 let shortPosition;
 let longTpOrderId;
 let longSlOrderId;
+const tickSize = tick * 1000;
 
 // Trading Logic
 app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
@@ -123,7 +127,7 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 					let quoteAssetPrice = await binance.fetchTicker (symbol).then(res => {
 						return res.last;
 					});
-					let quoteAmount = binance.amountToPrecision(symbol, (balanceBase * 0.975) / quoteAssetPrice);
+					let quoteAmount = await binance.amountToPrecision(symbol, (balanceBase * 0.975) / quoteAssetPrice);
 					// Open long market order
 					binance.createMarketBuyOrder(symbol, quoteAmount).then(res => {
 						tgbot.telegram.sendMessage(
@@ -144,14 +148,7 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 							balanceQuote = balances.free[quoteAsset];
 							balanceBase = balances.free[baseAsset];
 							totalValue = (balanceQuote * quoteAssetPrice) + balanceBase;
-							tgbot.telegram.sendMessage(
-								chatId,
-								"<u>BALANCES UPDATE</u>\n\n" +
-								"<b>ETH: </b><pre>" + balanceQuote + "</pre>\n" +
-								"<b>USDT: </b><pre>" + balanceBase + "</pre>\n\n" +
-								"<b>Total Value: </b><pre>" + totalValue + "</pre>",
-								{ parse_mode : 'HTML' }
-							)
+							
 							// Calc trade risk and create SL/TP orders
 							let fromTimestamp = binance.milliseconds() - 1800 * 1000;
 							binance.fetchOHLCV(symbol, '5m', fromTimestamp, 6).then(a => {
@@ -166,7 +163,7 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 									'quantity': binance.amountToPrecision(symbol, quoteAmount),
 									'price': binance.priceToPrecision(symbol, takeProfitPrice),
 									'stopPrice': binance.priceToPrecision(symbol, stopLossPrice),
-									'stopLimitPrice': binance.priceToPrecision(symbol, stopLossPrice * 0.9995),
+									'stopLimitPrice': binance.priceToPrecision(symbol, stopLossPrice * 0.9998),
 									'stopLimitTimeInForce': 'GTC'
 								}
 								binance.privatePostOrderOco(params).then(b => {
@@ -188,7 +185,11 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 										"<u>TAKE PROFIT OPENED</u>\n" +
 										"<b>Order ID: </b><pre>" + longTpOrderId + "</pre>\n" + 
 										"<b>TP Price: </b><pre>" + tpOrder.price + " USDT</pre>\n" +
-										"<b>Quantity: </b><pre>" + tpOrder.origQty + "</pre>\n\n",
+										"<b>Quantity: </b><pre>" + tpOrder.origQty + "</pre>\n\n" +
+										"<u>BALANCES UPDATE</u>\n\n" +
+										"<b>ETH: </b><pre>" + balanceQuote + "</pre>\n" +
+										"<b>USDT: </b><pre>" + balanceBase + "</pre>\n\n" +
+										"<b>Total Value: </b><pre>" + totalValue + "</pre>",
 										{ parse_mode : 'HTML' }
 									)
 								})
@@ -197,7 +198,7 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 					});
 				}) ();
 			} else {
-				// Insufficient USDT balance, likely existing long position open.
+				// Spot account base asset low, likely long position open. Block further longs and await strategy completion.
 				tgbot.telegram.sendMessage(
 					chatId,
 					"<u>TRADE FAILED</u>\n\n" +
@@ -205,28 +206,138 @@ app.post('/api/v1/' + quoteAsset + baseAsset, function (req, res) {
 					{ parse_mode : 'HTML' }
 				)
 			}
+
+			// Short signal confirmed
+			if (signal1 === 'short' && signal2 === 'short' && shortPosition !== 'open') {
+				if (marginBalanceBase > 5) { // Check sufficient base balance
+					(async () => {
+						marginBalanceBase = await binance.fetchBalance({ 'marginMode' : marginMode}).then(res => {
+							return res[symbol].free[baseAsset];
+						});
+						let quoteAssetPrice = await binance.fetchTicker(symbol).then(res => {
+							return res.last;
+						});
+					
+						// Borrow quote asset on margin market
+						let borrowQuoteAmount = await binance.amountToPrecision(symbol, (marginBalanceBase * shortLeverage * 0.975) / quoteAssetPrice);
+						binance.borrowMargin(quoteAsset, borrowQuoteAmount, symbol, { 'marginMode' : marginMode }).then(a => {
+							// Short sell quote asset
+							binance.createMarketSellOrder(symbol, borrowQuoteAmount, { 'marginMode' : marginMode }).then(b => {
+								binance.fetchBalance({ 'marginMode' : marginMode }).then(c => {
+									// Fetch margin balances after short position open
+									marginBalanceQuote = c[symbol].free[quoteAsset]
+									marginBalanceBase = c[symbol].free[baseAsset]
+									marginTotalValue = (marginBalanceQuote * quoteAssetPrice) + marginBalanceBase
+					
+									// Calc range highs and risk value
+									let fromTimestamp = binance.milliseconds() - 1800 * 1000;
+									binance.fetchOHLCV(symbol, '5m', fromTimestamp, 6).then(d => {
+										let candleHighs = [d[0][2],d[1][2],d[2][2],d[3][2],d[4][2],d[5][2]]
+										let rangeHigh = candleHighs.sort()[5];
+										let risk = rangeHigh - quoteAssetPrice
+										let stopLossPrice = quoteAssetPrice + (risk * riskFactor)
+										let takeProfitPrice = quoteAssetPrice - (risk * takeProfitFactor)
+					
+										// Open stop-loss limit order
+										binance.createOrder(
+											quoteAsset + baseAsset,
+											'STOP_LOSS_LIMIT',
+											'buy',
+											binance.amountToPrecision(symbol, borrowQuoteAmount * 1.001),
+											binance.priceToPrecision(symbol, stopLossPrice * 1.0002),
+											{
+												'stopPrice': binance.priceToPrecision(symbol, stopLossPrice),
+												'marginMode': marginMode
+											}
+										).then(slOrder => {
+											// Open take-profit limit order
+											binance.createOrder(
+												quoteAsset + baseAsset,
+												'TAKE_PROFIT_LIMIT',
+												'buy',
+												binance.amountToPrecision(symbol, borrowQuoteAmount * 1.001),
+												binance.priceToPrecision(symbol, takeProfitPrice * 1.0002),
+												{
+													'stopPrice': binance.priceToPrecision(symbol, takeProfitPrice),
+													'marginMode': marginMode
+												}
+											).then(tpOrder => {
+												// Store SL/TP order IDs and mark position open
+												shortSlOrderId = slOrder.id
+												shortTpOrderId = tpOrder.id
+												shortPosition = 'open'
+			
+												// Telegram reporting
+												tgbot.telegram.sendMessage(
+													chatId,
+													"<u>OPEN SHORT POSITION</u>\n\n" +
+													"<b>Market: </b><pre>" + b.symbol + "</pre>\n" +
+													"<b>Leverage: </b><pre>" + shortLeverage + "</pre>\n" +
+													"<b>Borrowed: </b><pre>" + borrowQuoteAmount + " " + quoteAsset + "</pre>\n" +
+													"<b>Executed: </b><pre>" + b.filled + " " + quoteAsset + "</pre>\n" +
+													"<b>TradeID: </b><pre>" + b.clientOrderId + "</pre>\n" +
+													"<b>Execution Timestamp: </b><pre>" + b.datetime + "</pre>\n" +
+													"<b>Status: </b><pre>" + b.info.status + "</pre>\n" +
+													"<b>Price: </b><pre>" + b.price + " " + baseAsset + "</pre>\n" +
+													"<b>Cost: </b><pre>" + b.cost + "</pre>\n\n" +
+													"Chart: https://www.tradingview.com/chart/w9Jx4CUu/",
+													{ parse_mode : 'HTML' , disable_web_page_preview : true }
+												)
+			
+												tgbot.telegram.sendMessage(
+													chatId,
+													"<u>SL/TP PARAMS</u>\n" +
+													"<b>Risk: </b><pre>" + risk + "</pre>\n" + 
+													"<b>SL Factor: </b><pre>" + riskFactor + "</pre>\n" +
+													"<b>TP Factor: </b><pre>" + takeProfitFactor + "</pre>\n\n" +
+													"<u>STOP LOSS OPENED</u>\n" +
+													"<b>Order ID: </b><pre>" + shortSlOrderId + "</pre>\n" + 
+													"<b>SL Price: </b><pre>" + slOrder.price + " USDT</pre>\n" +
+													"<b>Quantity: </b><pre>" + slOrder.amount + "</pre>\n\n" + 
+													"<u>TAKE PROFIT OPENED</u>\n" +
+													"<b>Order ID: </b><pre>" + shortTpOrderId + "</pre>\n" + 
+													"<b>TP Price: </b><pre>" + tpOrder.price + " USDT</pre>\n" +
+													"<b>Quantity: </b><pre>" + tpOrder.amount + "</pre>\n\n",
+													{ parse_mode : 'HTML' }
+												)
+											})
+										})
+									})
+								})
+							})
+						});
+					}) ();
+				} else {
+					// Margin account base asset low, likely short position open. Block further shorts and await strategy completion.
+					tgbot.telegram.sendMessage(
+						chatId,
+						"<u>TRADE FAILED</u>\n\n" +
+						"Short position active. Current margin balance is " + marginBalanceBase + " USDT. Awaiting close signal.\n\n",
+						{ parse_mode : 'HTML' }
+					)
+				}
+			}
 		}
-		
-		/** TO DO
-		 *  Add short trading logic and order creation with SL/TP
-		 */
-		// Short signal confirmed
-		// if (signal1 === 'short' && signal2 === 'short' && shortPosition !== 'open') {
-		// 	console.log("OPENING SHORT");
-		// 	shortPosition = 'open';
-		// 	longPosition = 'close';
-		// }
 	}
 })
 
+
 setInterval(() => {
+	// Update balances for spot and margin accounts
 	binance.fetchBalance().then(balances => {
 		balanceBase = balances.free[baseAsset]
 	})
+	binance.fetchBalance({ 'marginMode' : marginMode }).then(balances => {
+		marginBalanceBase = c[symbol].free[baseAsset]
+	})
+
+	// Check for active long positions
 	if (longPosition === 'open') {
+		// Check spot long TAKE-PROFIT triggered
 		binance.fetchOrder(longTpOrderId, symbol).then(a => {
 			if (a.status === 'closed') {
-				longPosition = 'closed'
+				// TP Triggered -> calc balances and pnl, then report to telegram
+				longPosition = 'closed' // Set long-position to closed
 				binance.fetchBalance().then(balances => {
 					balanceQuote = balances.free[quoteAsset];
 					balanceBase = balances.free[baseAsset];
@@ -234,7 +345,7 @@ setInterval(() => {
 					let pnl = parseFloat((totalValue - initialValue) / initialValue * 100).toFixed(2);
 					tgbot.telegram.sendMessage(
 						chatId,
-						"<u>TAKE PROFIT TRIGGERED</u>\n" +
+						"<u>LONG POSITION: TAKE-PROFIT TRIGGERED</u>\n" +
 						"<b>TP Price: </b><pre>" + a.price + "</pre>\n" +
 						"<b>Trigger Time: </b><pre>" + a.datetime + "</pre>\n\n" +
 						"<u>BALANCES UPDATE</u>\n" +
@@ -248,8 +359,9 @@ setInterval(() => {
 			}
 
 			if (a.status === 'expired') {
+				// SL Triggered -> calc balances and pnl, then report to telegram
 				binance.fetchOrder(longSlOrderId, symbol).then(b => {
-					longPosition = 'closed'
+					longPosition = 'closed' // Set long-position to closed
 					binance.fetchBalance().then(balances => {
 						balanceQuote = balances.free[quoteAsset];
 						balanceBase = balances.free[baseAsset];
@@ -257,7 +369,7 @@ setInterval(() => {
 						let pnl = parseFloat((totalValue - initialValue) / initialValue * 100).toFixed(2);
 						tgbot.telegram.sendMessage(
 							chatId,
-							"<u>STOP LOSS TRIGGERED</u>\n" +
+							"<u>LONG POSITION: STOP-LOSS TRIGGERED</u>\n" +
 							"<b>SL Price: </b><pre>" + b.price + "</pre>\n" +
 							"<b>Trigger Time: </b><pre>" + b.datetime + "</pre>\n\n" +
 							"<u>BALANCES UPDATE</u>\n" +
@@ -272,4 +384,81 @@ setInterval(() => {
 			}
 		})
 	}
-}, 10000)
+
+	// Check for active short-positions on margin
+	if (shortPosition === 'open') {
+		// Check margin short TAKE-PROFIT triggered
+		binance.fetchOrder(shortTpOrderId, symbol, { 'marginMode' : marginMode }).then(b => {
+			if (b.status === 'closed') {
+				// TP Triggered -> fetch borrowed amount and interest then repay
+				binance.fetchBorrowInterest(quoteAsset, symbol, undefined, 1, { 'marginMode' : marginMode }).then(c => {
+					let repayAmount = c[0].info.principal + res[0].info.interest
+					binance.repayMargin(quoteAsset, repayAmount, symbol, { 'marginMode': marginMode }).then(d => {
+						// Set short-position to closed
+						shortPosition = 'closed'
+	
+						// Cancel stop-loss limit order
+						binance.cancelOrder(shortSlOrderId, symbol, { 'marginMode' : marginMode })
+	
+						// Fetch margin balances, calc PNL, and report to Telegram
+						binance.fetchBalance({ 'marginMode' : marginMode }).then(e => {
+							marginBalanceBase = c[symbol].free[baseAsset]
+							marginBalanceQuote = c[symbol].free[quoteAsset]
+							marginTotalValue = (marginBalanceQuote * b.price) + marginBalanceBase
+							let pnl = parseFloat((marginTotalValue - marginInitialValue) / marginInitialValue * 100).toFixed(2)
+							tgbot.telegram.sendMessage(
+								chatId,
+								"<u>SHORT POSITION: TAKE-PROFIT TRIGGERED</u>\n" +
+								"<b>TP Price: </b><pre>" + b.price + "</pre>\n" +
+								"<b>Trigger Time: </b><pre>" + b.datetime + "</pre>\n\n" +
+								"<u>BALANCES UPDATE</u>\n" +
+								"<b>ETH: </b><pre>" + marginBalanceQuote + "</pre>\n" +
+								"<b>USDT: </b><pre>" + marginBalanceBase + "</pre>\n\n" +
+								"<b>Total Value: </b><pre>" + marginTotalValue + "</pre>\n\n" +
+								"<b>P&L: </b>" + pnl + "%",
+								{ parse_mode : 'HTML' }
+							)
+						})
+					})
+				})
+			}
+		})
+	
+		// Check margin short STOP-LOSS triggered
+		binance.fetchOrder(shortSlOrderId, symbol, { 'marginMode' : marginMode }).then(b => {
+			if (b.status === 'closed') {
+				// SL Triggered -> fetch borrowed amount and interest then repay
+				binance.fetchBorrowInterest(quoteAsset, symbol, undefined, 1, { 'marginMode' : marginMode }).then(c => {
+					let repayAmount = c[0].info.principal + res[0].info.interest
+					binance.repayMargin(quoteAsset, repayAmount, symbol, { 'marginMode': marginMode }).then(d => {
+						// Set short-position to closed
+						shortPosition = 'closed'
+	
+						// Cancel take-profit limit order
+						binance.cancelOrder(shortTpOrderId, symbol, { 'marginMode' : marginMode })
+	
+						// Fetch margin balances, calc PNL, and report to Telegram
+						binance.fetchBalance({ 'marginMode' : marginMode }).then(e => {
+							marginBalanceBase = c[symbol].free[baseAsset]
+							marginBalanceQuote = c[symbol].free[quoteAsset]
+							marginTotalValue = (marginBalanceQuote * b.price) + marginBalanceBase
+							let pnl = parseFloat((marginTotalValue - marginInitialValue) / marginInitialValue * 100).toFixed(2)
+							tgbot.telegram.sendMessage(
+								chatId,
+								"<u>SHORT POSITION: STOP-LOSS TRIGGERED</u>\n" +
+								"<b>SL Price: </b><pre>" + b.price + "</pre>\n" +
+								"<b>Trigger Time: </b><pre>" + b.datetime + "</pre>\n\n" +
+								"<u>BALANCES UPDATE</u>\n" +
+								"<b>ETH: </b><pre>" + marginBalanceQuote + "</pre>\n" +
+								"<b>USDT: </b><pre>" + marginBalanceBase + "</pre>\n\n" +
+								"<b>Total Value: </b><pre>" + marginTotalValue + "</pre>\n\n" +
+								"<b>P&L: </b>" + pnl + "%",
+								{ parse_mode : 'HTML' }
+							)
+						})
+					})
+				})
+			}
+		})
+	}
+}, tickSize)
--- test_files/test.js
+++ test_files/test.js
@@ -2,15 +2,15 @@ const ccxt = require("ccxt");
 require ('dotenv').config();
 
 let binance = new ccxt.binance ({
-	'apiKey': process.env.BINANCE_TEST_APIKEY,
-	'secret': process.env.BINANCE_TEST_SECRET,
+	'apiKey': process.env.BINANCE_APIKEY,
+	'secret': process.env.BINANCE_SECRET,
 	'enablerateLimit': true,
 	'options': {
 		'createMarketBuyOrderRequiresPrice': false
 	}
 });
 
-binance.setSandboxMode(true);
+// binance.setSandboxMode(true);
 
 let quoteAsset = 'ETH'
 let baseAsset = 'USDT'
@@ -25,78 +25,6 @@ binance.loadMarkets()
 let testSlOrderId;
 let testTpOrderId;
 let longPosition;
-
-let params = {
-	'symbol': quoteAsset + baseAsset,
-	'side': 'SELL',
-	'quantity': 0.5,
-	'price': 1903,
-	'stopPrice': 1900,
-	'stopLimitPrice': 1500 * 0.999,
-	'stopLimitTimeInForce': 'GTC'
-}
-
-binance.privatePostOrderOco(params).then(res => {
-	testSlOrderId = res.orderReports[0].orderId
-	testTpOrderId = res.orderReports[1].orderId
-	longPosition = 'open'
-	console.log('==== STOP LOSS ====')
-	console.log('Order ID: ' + testSlOrderId)
-	console.log('=== TAKE PROFIT ===')
-	console.log('Order ID: ' + testTpOrderId)
-	binance.fetchOrder(testSlOrderId, symbol).then(res2 => {
-		console.log(" ")
-		console.log("=== sl order check ===")
-		console.log(res2)
-	})
-	binance.fetchOrder(testTpOrderId, symbol).then(res2 => {
-		console.log(" ")
-		console.log("=== tp order check ===")
-		console.log(res2)
-	})
-})
-
-function checkOrder() {
-	if (longPosition === 'open') {
-		binance.fetchOrder(testTpOrderId, symbol).then(res => {
-			if (res.status === 'closed') {
-				longPosition = 'closed'
-				console.log('Take Profit Triggered!')
-				console.log('TP Price: ' + res.price)
-			}
-
-			if (res.status === 'expired') {
-				binance.fetchOrder(testSlOrderId, symbol).then(a => {
-					longPosition = 'closed'
-					console.log('Stop Loss Triggered!')
-					console.log('SL Price: ' + a.price)
-				})
-			}
-		})
-	} else {
-		return
-	}
-}
-
-setInterval(() => {
-	if (longPosition === 'open') {
-		binance.fetchOrder(testTpOrderId, symbol).then(res => {
-			if (res.status === 'closed') {
-				longPosition = 'closed'
-				console.log('Take Profit Triggered!')
-				console.log('TP Price: ' + res.price)
-			}
-
-			if (res.status === 'expired') {
-				binance.fetchOrder(testSlOrderId, symbol).then(a => {
-					longPosition = 'closed'
-					console.log('Stop Loss Triggered!')
-					console.log('SL Price: ' + a.price)
-				})
-			}
-		})
-	}
-}, 1000)
 
 // binance.fetchOrder(testOrderId, symbol).then(res => {
 // 	console.log(res)
@@ -151,3 +79,210 @@ setInterval(() => {
 // }) ();
 
 // binance.fetchPositions()
+
+let marginBalanceBase;
+let marginBalanceQuote;
+let marginInitialValue;
+let marginTotalValue;
+let shortTpOrderId;
+let shortSlOrderId;
+let marginMode = 'isolated';
+let shortLeverage = 1
+
+
+if (signal1 === 'short' && signal2 === 'short' && shortPosition !== 'open') {
+	if (marginBalanceBase > 5) { // Check sufficient base balance
+		(async () => {
+			marginBalanceBase = await binance.fetchBalance({ 'marginMode' : marginMode}).then(res => {
+				return res[symbol].free[baseAsset];
+			});
+			let quoteAssetPrice = await binance.fetchTicker(symbol).then(res => {
+				return res.last;
+			});
+		
+			// Borrow quote asset on margin market
+			let borrowQuoteAmount = await binance.amountToPrecision(symbol, (marginBalanceBase * shortLeverage * 0.975) / quoteAssetPrice);
+			binance.borrowMargin(quoteAsset, borrowQuoteAmount, symbol, { 'marginMode' : marginMode }).then(a => {
+				// Short sell quote asset
+				binance.createMarketSellOrder(symbol, borrowQuoteAmount, { 'marginMode' : marginMode }).then(b => {
+					binance.fetchBalance({ 'marginMode' : marginMode }).then(c => {
+						// Fetch margin balances after short position open
+						marginBalanceQuote = c[symbol].free[quoteAsset]
+						marginBalanceBase = c[symbol].free[baseAsset]
+						marginTotalValue = (marginBalanceQuote * quoteAssetPrice) + marginBalanceBase
+		
+						// Calc range highs and risk value
+						let fromTimestamp = binance.milliseconds() - 1800 * 1000;
+						binance.fetchOHLCV(symbol, '5m', fromTimestamp, 6).then(d => {
+							let candleHighs = [d[0][2],d[1][2],d[2][2],d[3][2],d[4][2],d[5][2]]
+							let rangeHigh = candleHighs.sort()[5];
+							let risk = rangeHigh - quoteAssetPrice
+							let stopLossPrice = quoteAssetPrice + (risk * riskFactor)
+							let takeProfitPrice = quoteAssetPrice - (risk * takeProfitFactor)
+		
+							// Open stop-loss limit order
+							binance.createOrder(
+								quoteAsset + baseAsset,
+								'STOP_LOSS_LIMIT',
+								'buy',
+								binance.amountToPrecision(symbol, borrowQuoteAmount * 1.001),
+								binance.priceToPrecision(symbol, stopLossPrice * 1.0002),
+								{
+									'stopPrice': binance.priceToPrecision(symbol, stopLossPrice),
+									'marginMode': marginMode
+								}
+							).then(slOrder => {
+								// Open take-profit limit order
+								binance.createOrder(
+									quoteAsset + baseAsset,
+									'TAKE_PROFIT_LIMIT',
+									'buy',
+									binance.amountToPrecision(symbol, borrowQuoteAmount * 1.001),
+									binance.priceToPrecision(symbol, takeProfitPrice * 1.0002),
+									{
+										'stopPrice': binance.priceToPrecision(symbol, takeProfitPrice),
+										'marginMode': marginMode
+									}
+								).then(tpOrder => {
+									// Store SL/TP order IDs and mark position open
+									shortSlOrderId = slOrder.id
+									shortTpOrderId = tpOrder.id
+									shortPosition = 'open'
+
+									// Store new balances after SL/TP positions opened
+									binance.fetchBalance({ 'marginMode' : marginMode }).then(e => {
+										marginBalanceQuote = c[symbol].free[quoteAsset]
+									})
+
+									// Telegram reporting
+									tgbot.telegram.sendMessage(
+										chatId,
+										"<u>OPEN SHORT POSITION</u>\n\n" +
+										"<b>Market: </b><pre>" + b.symbol + "</pre>\n" +
+										"<b>Leverage: </b><pre>" + shortLeverage + "</pre>\n" +
+										"<b>Borrowed: </b><pre>" + borrowQuoteAmount + " " + quoteAsset + "</pre>\n" +
+										"<b>Executed: </b><pre>" + b.filled + " " + quoteAsset + "</pre>\n" +
+										"<b>TradeID: </b><pre>" + b.clientOrderId + "</pre>\n" +
+										"<b>Execution Timestamp: </b><pre>" + b.datetime + "</pre>\n" +
+										"<b>Status: </b><pre>" + b.info.status + "</pre>\n" +
+										"<b>Price: </b><pre>" + b.price + " " + baseAsset + "</pre>\n" +
+										"<b>Cost: </b><pre>" + b.cost + "</pre>\n\n" +
+										"Chart: https://www.tradingview.com/chart/w9Jx4CUu/",
+										{ parse_mode : 'HTML' , disable_web_page_preview : true }
+									)
+
+									tgbot.telegram.sendMessage(
+										chatId,
+										"<u>SL/TP PARAMS</u>\n" +
+										"<b>Risk: </b><pre>" + risk + "</pre>\n" + 
+										"<b>SL Factor: </b><pre>" + riskFactor + "</pre>\n" +
+										"<b>TP Factor: </b><pre>" + takeProfitFactor + "</pre>\n\n" +
+										"<u>STOP LOSS OPENED</u>\n" +
+										"<b>Order ID: </b><pre>" + shortSlOrderId + "</pre>\n" + 
+										"<b>SL Price: </b><pre>" + slOrder.price + " USDT</pre>\n" +
+										"<b>Quantity: </b><pre>" + slOrder.amount + "</pre>\n\n" + 
+										"<u>TAKE PROFIT OPENED</u>\n" +
+										"<b>Order ID: </b><pre>" + shortTpOrderId + "</pre>\n" + 
+										"<b>TP Price: </b><pre>" + tpOrder.price + " USDT</pre>\n" +
+										"<b>Quantity: </b><pre>" + tpOrder.amount + "</pre>\n\n",
+										{ parse_mode : 'HTML' }
+									)
+								})
+							})
+						})
+					})
+				})
+			});
+		}) ();
+	} else {
+		// Margin account base asset low, likely short position open. Block further shorts and await strategy completion.
+		tgbot.telegram.sendMessage(
+			chatId,
+			"<u>TRADE FAILED</u>\n\n" +
+			"Short position active. Current margin balance is " + marginBalanceBase + " USDT. Awaiting close signal.\n\n",
+			{ parse_mode : 'HTML' }
+		)
+	}
+}
+
+
+let riskFactor = 1
+let takeProfitFactor = 2
+
+
+
+if (shortPosition === 'open') {
+	// Check margin short TAKE-PROFIT triggered
+	binance.fetchOrder(shortTpOrderId, symbol, { 'marginMode' : marginMode }).then(b => {
+		if (b.status === 'closed') {
+			// TP Triggered -> fetch borrowed amount and interest then repay
+			binance.fetchBorrowInterest(quoteAsset, symbol, undefined, 1, { 'marginMode' : marginMode }).then(c => {
+				let repayAmount = c[0].info.principal + res[0].info.interest
+				binance.repayMargin(quoteAsset, repayAmount, symbol, { 'marginMode': marginMode }).then(d => {
+					// Set short-position to closed
+					shortPosition = 'closed'
+
+					// Cancel stop-loss limit order
+					binance.cancelOrder(shortSlOrderId, symbol, { 'marginMode' : marginMode })
+
+					// Fetch margin balances, calc PNL, and report to Telegram
+					binance.fetchBalance({ 'marginMode' : marginMode }).then(e => {
+						marginBalanceBase = c[symbol].free[baseAsset]
+						marginBalanceQuote = c[symbol].free[quoteAsset]
+						marginTotalValue = (marginBalanceQuote * b.price) + marginBalanceBase
+						let pnl = parseFloat((marginTotalValue - marginInitialValue) / marginInitialValue * 100).toFixed(2)
+						tgbot.telegram.sendMessage(
+							chatId,
+							"<u>SHORT POSITION: TAKE-PROFIT TRIGGERED</u>\n" +
+							"<b>TP Price: </b><pre>" + b.price + "</pre>\n" +
+							"<b>Trigger Time: </b><pre>" + b.datetime + "</pre>\n\n" +
+							"<u>BALANCES UPDATE</u>\n" +
+							"<b>ETH: </b><pre>" + marginBalanceQuote + "</pre>\n" +
+							"<b>USDT: </b><pre>" + marginBalanceBase + "</pre>\n\n" +
+							"<b>Total Value: </b><pre>" + marginTotalValue + "</pre>\n\n" +
+							"<b>P&L: </b>" + pnl + "%",
+							{ parse_mode : 'HTML' }
+						)
+					})
+				})
+			})
+		}
+	})
+
+	// Check margin short STOP-LOSS triggered
+	binance.fetchOrder(shortSlOrderId, symbol, { 'marginMode' : marginMode }).then(b => {
+		if (b.status === 'closed') {
+			// SL Triggered -> fetch borrowed amount and interest then repay
+			binance.fetchBorrowInterest(quoteAsset, symbol, undefined, 1, { 'marginMode' : marginMode }).then(c => {
+				let repayAmount = c[0].info.principal + res[0].info.interest
+				binance.repayMargin(quoteAsset, repayAmount, symbol, { 'marginMode': marginMode }).then(d => {
+					// Set short-position to closed
+					shortPosition = 'closed'
+
+					// Cancel take-profit limit order
+					binance.cancelOrder(shortTpOrderId, symbol, { 'marginMode' : marginMode })
+
+					// Fetch margin balances, calc PNL, and report to Telegram
+					binance.fetchBalance({ 'marginMode' : marginMode }).then(e => {
+						marginBalanceBase = c[symbol].free[baseAsset]
+						marginBalanceQuote = c[symbol].free[quoteAsset]
+						marginTotalValue = (marginBalanceQuote * b.price) + marginBalanceBase
+						let pnl = parseFloat((marginTotalValue - marginInitialValue) / marginInitialValue * 100).toFixed(2)
+						tgbot.telegram.sendMessage(
+							chatId,
+							"<u>SHORT POSITION: STOP-LOSS TRIGGERED</u>\n" +
+							"<b>SL Price: </b><pre>" + b.price + "</pre>\n" +
+							"<b>Trigger Time: </b><pre>" + b.datetime + "</pre>\n\n" +
+							"<u>BALANCES UPDATE</u>\n" +
+							"<b>ETH: </b><pre>" + marginBalanceQuote + "</pre>\n" +
+							"<b>USDT: </b><pre>" + marginBalanceBase + "</pre>\n\n" +
+							"<b>Total Value: </b><pre>" + marginTotalValue + "</pre>\n\n" +
+							"<b>P&L: </b>" + pnl + "%",
+							{ parse_mode : 'HTML' }
+						)
+					})
+				})
+			})
+		}
+	})
+}
